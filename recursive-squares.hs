2ffd44cb69a0686aeb521b89381e4c12675dd3bb
coreen <coreeny@cs.washington.edu>
2014-01-03 10:14:15 -0800

    Update recursive-squares.hs

--- recursive-squares.hs
+++ recursive-squares.hs
@@ -3,3 +3,5 @@
 -- recursively squares a list of integers
 recursive-squares :: [Integer] -> [Integer]
 recursive-squares (x:xs) = (x ^ 2) : squares xs
+-- empty list edge case
+recursive-squares [] = []
