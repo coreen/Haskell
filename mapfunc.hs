5cca688a26340b67c08a8aee4a45f67a2808c263
coreen <coreeny@cs.washington.edu>
2014-01-09 15:24:36 -0800

    Added examples of usage.

--- mapfunc.hs
+++ mapfunc.hs
@@ -1,6 +1,11 @@
 -- Coreen Yuen
+
 -- mapfunc takes a list of functions and a list of items
 -- returns a new list resulted from applying each function to its corresponding index
+
+-- examples of usage:
+--    mapfuncs [(+7), (+5)] [1..] => [8, 7]
+--    mapfuncs [(-1), (*2), sqrt] [2.0, 3.5, 4.0] => [1.0, 7.0, 2.0]
 
 mapfuncs :: [t -> a] -> [t] -> [a]
 mapfuncs (f:fs) (x:xs) = f x : mapfuncs fs xs
